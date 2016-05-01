649286d4346010f459a850a086c1c242c974cd3c
Paul Waldo <paulwwaldo@gmail.com>
2016-05-01 17:57:16 -0400

    Brand new grails angular app

---
+++ grails-app/assets/javascripts/angello/angello.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /angello/core/angello.core
+//= require /angello/index/angello.index
+
+angular.module("angello", [
+    "angello.core",
+    "angello.index"
+]);
