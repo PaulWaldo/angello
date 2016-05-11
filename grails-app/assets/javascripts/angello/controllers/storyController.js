227e5a2b222c8816d6e857d51edc7f6392a0cba3
Paul Waldo <paulwwaldo@gmail.com>
2016-05-11 05:00:33 -0400

    Implemented story CRUD

--- grails-app/assets/javascripts/angello/controllers/storyController.js
+++ grails-app/assets/javascripts/angello/controllers/storyController.js
@@ -6,5 +6,24 @@ angular
 
 function StoryController() {
     var vm = this;
-    vm.stories
+    vm.stories = [];
+    vm.newStory = new Story();
+
+    vm.save = function() {
+        vm.newStory.$save({}, function() {
+            vm.stories.push(angular.copy(vm.newStory));
+            vm.newStory = new Story();
+        });
+    }
+
+    vm.delete = function(story) {
+        story.$delete({}, function() {
+            var idx = vm.stories.indexOf(story);
+            vm.stories.splice(idx, 1);
+        });
+    }
+
+    vm.update = function(story) {
+        story.$update();
+    }
 }
