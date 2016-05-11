6d0e13c7cb4b8d5224fde459cf77b08fa8a60fd0
Paul Waldo <paulwwaldo@gmail.com>
2016-05-11 15:06:18 -0400

    Added Story to default data set

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,22 +1,33 @@
 import angello.Person
 import angello.Status
+import angello.Story
 import angello.StoryType
 
 class BootStrap {
 
     def init = { servletContext ->
-        new Person(firstName: 'Fred', lastName: 'Smith').save(failOnError: true)
+        def p1 = new Person(firstName: 'Fred', lastName: 'Smith').save(failOnError: true)
+        def p2 = new Person(firstName: 'Sally', lastName: 'Joe').save(failOnError: true)
 
-        new Status(name: 'To Do').save(failOnError: true)
+        def todo = new Status(name: 'To Do').save(failOnError: true)
         new Status(name: 'In Progress').save(failOnError: true)
         new Status(name: 'Code Review').save(failOnError: true)
         new Status(name: 'QA Review').save(failOnError: true)
         new Status(name: 'Verified').save(failOnError: true)
 
-        new StoryType(name: 'Feature').save(failOnError: true)
+        def feature = new StoryType(name: 'Feature').save(failOnError: true)
         new StoryType(name: 'Enhancement').save(failOnError: true)
         new StoryType(name: 'Bug').save(failOnError: true)
         new StoryType(name: 'Spike').save(failOnError: true)
+
+        new Story(
+            title: 'My first story',
+            description: 'The first one',
+            assignee: p1,
+            type: feature,
+            status:todo,
+            criteria: 'It just works',
+            reporter: p2).save(failOnError: true)
     }
     def destroy = {
     }
